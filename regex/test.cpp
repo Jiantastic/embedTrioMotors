427b9e602717476b92513e0cf47284b3016742b2
Jiantastic <srswwj@gmail.com>
2017-03-06 20:19:28 +0000

    updates

---
+++ regex/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "slre.h"
+
+using namespace std;
+
+int main(){
+	const char *request = " GET /index.html HTTP/1.0\r\n\r\n";
+	struct slre_cap caps[4];
+
+	if (slre_match("^\\s*(\\S+)\\s+(\\S+)\\s+HTTP/(\\d)\\.(\\d)",
+				request, strlen(request), caps, 4, 0) > 0) {
+	printf("Method: [%.*s], URI: [%.*s]\n",
+			caps[0].len, caps[0].ptr,
+			caps[1].len, caps[1].ptr);
+	} else {
+	printf("Error parsing [%s]\n", request);
+	}
+	return 0;
+}
