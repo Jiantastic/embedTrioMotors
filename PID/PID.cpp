d5ccfbf239d7656a1ebd7d9fcd1c77c430eaf084
junweisow789 <junweisow789@gmail.com>
2017-03-09 13:11:35 +0000

    Update PID.cpp

--- PID/PID.cpp
+++ PID/PID.cpp
@@ -9,8 +9,8 @@ PID::PID(float Kc, float tauI, float tauD, float interval) {
     //Make sure to set these to more appropriate limits for
     //your application.
     //!!!!!!!!!!!!change this!!!!!!!!!!
-    setInputLimits(0.0, 3.3);
-    setOutputLimits(0.0, 3.3);
+    setInputLimits(0.0, R*117);
+    setOutputLimits(0.0, 3.0);
     //setInputLimits(0.0, 117.0); // 117 is the max pulsecount per rotation
     //setOutputLimits(0.0, 3.0);  //Vmotor = 3V
  
